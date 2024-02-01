caea278d0eb3abc0e7bf2cc01e4c352bf5348e4a
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2024-02-01 21:54:47 +0300

    Delete todo-rest-api directory

--- todo-rest-api/src/main/java/com/example/todorestapi/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todorestapi.repositories;
-
-import com.example.todorestapi.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
